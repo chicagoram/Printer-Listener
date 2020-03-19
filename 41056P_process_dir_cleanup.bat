35feeb5a57a7db39884143afc8035786df41b5fd
chicagoram <ram_1726@yahoo.com>
2020-03-19 14:48:31 -0500

    modified

--- 41056P_process_dir_cleanup.bat
+++ 41056P_process_dir_cleanup.bat
@@ -1,3 +1,6 @@
 @echo off
-forfiles /p "C:\Processed_41056P" /s /m *.tif /D -5 /C "cmd /c del @path"
+forfiles /p "C:\Processed_41056P" /s /m *.* /D -2 /C "cmd /c del @path"
+forfiles /p "C:\41056H\[_EFI_HotFolder_]\[MoveFolder]" /s /m *.* /D -2 /C "cmd /c del @path"
+pause
+
 
